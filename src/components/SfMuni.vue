5763723a7a937c0785f930b39975a906c6e89f80
neha <neha.thiyagarajan@linqia.com>
2017-11-14 15:23:43 -0800

    formatting changes

--- src/components/SfMuni.vue
+++ src/components/SfMuni.vue
@@ -14,10 +14,10 @@
 </template>
 
 <script>
-  'use strict'
-  import * as d3 from 'd3'
-  import { getMuniRouteConfig, getMuniBusData } from '../helpers/api.js'
-  import { drawSfMap } from '../helpers/maps'
+  'use strict';
+  import * as d3 from 'd3';
+  import { getMuniRouteConfig, getMuniBusData } from '../helpers/api.js';
+  import { drawSfMap } from '../helpers/maps';
 
   export default {
     components: {},
@@ -30,18 +30,18 @@
       }
     },
     mounted: function () {
-      const width = 1000
-      const height = 500
-      d3.select('svg').remove()
-      this.projection = d3.geoMercator().scale(200000).center([-122.45, 37.75])
-      const path = d3.geoPath().projection(this.projection)
+      const width = 1000;
+      const height = 500;
+      d3.select('svg').remove();
+      this.projection = d3.geoMercator().scale(200000).center([-122.45, 37.75]);
+      const path = d3.geoPath().projection(this.projection);
       this.svg = d3.select('.map-container').append('svg')
         .attr('width', width)
-        .attr('height', height)
+        .attr('height', height);
 
       // draw the map of SF using the data in geoJSON format
-      drawSfMap(this.svg, path)
-      this.fetchRoute(this.svg, this.projection)
+      drawSfMap(this.svg, path);
+      this.fetchRoute(this.svg, this.projection);
 
       // calling the api to fetch the Bus location every 15 seconds after the route is rendered
       setInterval(function () {
@@ -50,26 +50,26 @@
     },
     computed: {
       routeList: function () {
-        let arr = []
+        let arr = [];
         for (let route of this.routes) {
-          arr.push({tag: route.tag, color: route.color})
+          arr.push({tag: route.tag, color: route.color});
           this.renderRoute(route)
         }
-        return arr
+        return arr;
       }
     },
     methods: {
       fetchRoute: function () {
-        var self = this
+        var self = this;
         getMuniRouteConfig().then(response => {
-          self.routes = response.data.route
+          self.routes = response.data.route;
           self.fetchBusData()
         }).catch(e => {
           console.log(e)
-        })
+        });
       },
       renderRoute: function (routeData) {
-        var self = this
+        var self = this;
         let routeLineFunction = d3.line()
           .x(function (d) {
             return self.projection([d.lon, d.lat])[0]
@@ -77,8 +77,8 @@
           .y(function (d) {
             return self.projection([d.lon, d.lat])[1]
           })
-          .curve(d3.curveLinear)
-        let tag = routeData.tag
+          .curve(d3.curveLinear);
+        let tag = routeData.tag;
         for (let value of routeData.path) {
           self.svg.append('path')
             .attr('d', routeLineFunction(value.point))
@@ -93,17 +93,14 @@
         }
       },
       fetchBusData: function () {
-        var self = this
-        self.store = []
+        var self = this;
+        self.store = [];
         for (const route of self.routes) {
-          getMuniBusData(route.tag).then(
-            function (bus) {
-              self.formatBusData(bus)
-            })
+          getMuniBusData(route.tag).then(bus => { self.formatBusData(bus)});
         }
       },
       formatBusData: function (bus) {
-        var self = this
+        var self = this;
         if (bus.data.hasOwnProperty('vehicle')) {
           for (let vehicle of bus.data.vehicle) {
             if (vehicle) {
@@ -111,38 +108,37 @@
             }
           }
         }
-        self.drawBus()
+        self.drawBus();
       },
       selectRoute: function () {
         if (this.selectedRoute.length === 0) {
           return
         }
-        d3.selectAll('.map').style('stroke-opacity', 0.3)
-        d3.selectAll('.route').style('stroke-opacity', 0.05).attr('stroke-width', 2)
+        d3.selectAll('.map').style('stroke-opacity', 0.3);
+        d3.selectAll('.route').style('stroke-opacity', 0.05).attr('stroke-width', 2);
 
         for (let route of this.selectedRoute) {
-          var routeTag = '.route[data-tag="' + route + '"]'
-          d3.selectAll(routeTag).style('stroke-opacity', 1).attr('stroke-width', 5)
+          const routeTag = '.route[data-tag="' + route + '"]';
+          d3.selectAll(routeTag).style('stroke-opacity', 1).attr('stroke-width', 5);
         }
-        d3.selectAll('.bus').style('fill-opacity', 0.5)
+        d3.selectAll('.bus').style('fill-opacity', 0.5);
         for (let selectRoute of this.selectedRoute) {
-          var buses = '.bus[data-route-tag="' + selectRoute + '"]'
+          const buses = '.bus[data-route-tag="' + selectRoute + '"]';
           d3.selectAll(buses).style('fill-opacity', 1)
         }
       },
       editRoute: function (routeTag) {
         // if the route is not in the model then remove the focus on the route
         if (this.selectedRoute.indexOf(routeTag) < 0) {
-          const route = '.route[data-tag="' + routeTag + '"]'
-          const buses = '.bus[data-route-tag="' + routeTag + '"]'
+          const route = '.route[data-tag="' + routeTag + '"]';
+          const buses = '.bus[data-route-tag="' + routeTag + '"]';
           if (this.selectedRoute.length === 0) {
-            d3.selectAll('.map').style('stroke-opacity', 0.8)
-            d3.selectAll('.route').style('stroke-opacity', 1).attr('stroke-width', 2)
-            d3.selectAll('.bus').style('fill-opacity', 0.75)
+            d3.selectAll('.map').style('stroke-opacity', 0.8);
+            d3.selectAll('.route').style('stroke-opacity', 1).attr('stroke-width', 2);
+            d3.selectAll('.bus').style('fill-opacity', 0.75);
           } else {
-            d3.selectAll(route).style('stroke-opacity', 0.05).attr('stroke-width', 2)
-            d3.selectAll(buses).style('opacity', 0.50)
-
+            d3.selectAll(route).style('stroke-opacity', 0.05).attr('stroke-width', 2);
+            d3.selectAll(buses).style('opacity', 0.50);
           }
         }
         else {
@@ -151,7 +147,7 @@
 
       },
       drawBus () {
-        var self = this
+        let self = this;
         const projection = d3.geoMercator().scale(200000).center([-122.45, 37.75])
         self.svg.selectAll('.bus')
           .data(self.store)
@@ -161,25 +157,12 @@
           .attr('fill', '#31558d')
           .style('fill-opacity', 0.5)
           .attr('class', 'bus')
-          .attr('data-route-tag', function (d) {
-            return d.routeTag
-          })
-          .attr('data-dir-tag', function (d) {
-            return d.dirTag
-          })
-          .attr('data-heading', function (d) {
-            return d.heading
-          })
-          .attr('data-id', function (d) {
-            return d.id
-          })
-          .attr('cx', function (d) {
-            return projection([d.lon, d.lat])[0]
-          })
-          .attr('cy', function (d) {
-            return projection([d.lon, d.lat])[1]
-          })
-
+          .attr('data-route-tag', d => d.routeTag)
+          .attr('data-dir-tag', d => d.dirTag)
+          .attr('data-heading', d => d.heading)
+          .attr('data-id', d => d.id)
+          .attr('cx', d => projection([d.lon, d.lat])[0])
+          .attr('cy', d => projection([d.lon, d.lat])[1])
       }
     }
   }
